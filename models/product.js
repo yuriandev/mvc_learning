100549c63276c9a6a1106c14c4d68023dc44c110
Yuri An <yuriandev@gmail.com>
2020-10-21 19:55:30 +0300

    Hello mysql)

--- models/product.js
+++ models/product.js
@@ -10,7 +10,8 @@ module.exports = class Product {
     this.price = price;
   }
   save() {
-    
+    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', 
+    [this.title, this.price, this.imageUrl, this.description]);
   }
 
   static deleteById(id) {
@@ -22,6 +23,6 @@ module.exports = class Product {
   }
 
   static findById(id) {
-    
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
 };
